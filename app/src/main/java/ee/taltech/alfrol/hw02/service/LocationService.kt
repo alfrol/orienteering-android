c52cb42d25632c5c9aa083e5461c5bab5441f108
Alexander <alfrol@ttu.ee>
2021-05-26 14:26:03 +0300

    hw02 - start implementing a location service

---
+++ app/src/main/java/ee/taltech/alfrol/hw02/service/LocationService.kt
@@ -0,0 +1,133 @@
+package ee.taltech.alfrol.hw02.service
+
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.os.Looper
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavDeepLinkBuilder
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
+import dagger.hilt.android.AndroidEntryPoint
+import ee.taltech.alfrol.hw02.C
+import ee.taltech.alfrol.hw02.R
+import ee.taltech.alfrol.hw02.data.SettingsManager
+import ee.taltech.alfrol.hw02.ui.fragments.SessionFragment
+import ee.taltech.alfrol.hw02.ui.utils.UIUtils
+import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class LocationService : LifecycleService() {
+
+    companion object {
+        val points = MutableLiveData<MutableList<LatLng>>(mutableListOf())
+    }
+
+    @Inject
+    lateinit var settingsManager: SettingsManager
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var notificationManager: NotificationManager
+
+    override fun onCreate() {
+        super.onCreate()
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        intent?.let {
+            when (it.action) {
+                C.ACTION_START_SERVICE -> startService()
+                C.ACTION_STOP_SERVICE -> stopService()
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun startService() {
+        startForeground(C.NOTIFICATION_ID, createNotification())
+        requestLocationUpdates()
+    }
+
+    private fun stopService() {
+        stopSelf()
+        fusedLocationProviderClient.removeLocationUpdates(callback)
+    }
+
+    /**
+     * Callback for receiving and processing the location updates.
+     */
+    private val callback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            addPoint(locationResult.lastLocation)
+        }
+    }
+
+    /**
+     * Start requesting the location updates.
+     */
+    @SuppressLint("MissingPermission")
+    private fun requestLocationUpdates() {
+        if (UIUtils.hasLocationPermission(this)) {
+            lifecycleScope.launchWhenCreated {
+                val interval = settingsManager.locationUpdateInterval.first()
+                val fastestInterval = settingsManager.locationUpdateFastestInterval.first()
+
+                val request = UIUtils.getLocationRequest(interval, fastestInterval)
+
+                fusedLocationProviderClient.requestLocationUpdates(
+                    request,
+                    callback,
+                    Looper.getMainLooper()
+                )
+            }
+        }
+    }
+
+    /**
+     * Add a new location point to the path point list.
+     */
+    private fun addPoint(location: Location?) {
+        location?.let {
+            val latLng = LatLng(it.latitude, it.longitude)
+            points.value?.apply {
+                add(latLng)
+                points.value = this
+            }
+        }
+    }
+
+    /**
+     * Create a new notification.
+     */
+    private fun createNotification(): Notification {
+        return NotificationCompat.Builder(this, C.NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText("")
+            .setContentIntent(createPendingIntent())
+            .build()
+    }
+
+    /**
+     * Create a pending intent for use in notification.
+     * It will be used for opening the [SessionFragment]
+     * when the user clicks on the notification.
+     */
+    private fun createPendingIntent() = NavDeepLinkBuilder(this)
+        .setGraph(R.navigation.nav_graph)
+        .setDestination(R.id.sessionFragment)
+        .createPendingIntent()
+}
